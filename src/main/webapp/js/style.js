3e8a7b2b3202b48d6877ec7ba93d28c911653120
timkeane <tkeane@doitt.nyc.gov>
2017-10-13 08:08:25 -0400

    remove debug

--- src/main/webapp/js/style.js
+++ src/main/webapp/js/style.js
@@ -4,13 +4,12 @@ var finderStyle = function(feature, resolution){
   var zoom = nyc.ol.TILE_GRID.getZForResolution(resolution);
   if (!finderStyle.cache[zoom]){
     var radius = zoom * .75, stroke = 1;
-    console.info(zoom,radius);
     finderStyle.cache[zoom] = [
       new ol.style.Style({
         image: new ol.style.Circle({
           radius: radius,
           fill: new ol.style.Fill({
-            color: '#1259a7'//'rgba(255,165,0,0.5)'
+            color: '#1259a7'
           }),
           stroke: new ol.style.Stroke({
             color: '#fff',
